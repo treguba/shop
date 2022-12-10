ff80708c0480e5916dfb5129e065abffe0a00c3d
treguba <120250120+treguba@users.noreply.github.com>
2022-12-10 21:18:48 +1000

    Initial commit

---
+++ shop/SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/controllers/MainController.java
@@ -0,0 +1,218 @@
+package com.example.springsecurityapplication.controllers;
+
+import com.example.springsecurityapplication.repositories.ProductRepository;
+import com.example.springsecurityapplication.security.PersonDetails;
+import com.example.springsecurityapplication.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/product")
+public class MainController {
+
+
+    private final ProductRepository productRepository;
+    private final ProductService productService;
+
+    @Autowired
+    public MainController(ProductRepository productRepository, ProductService productService) {
+        this.productRepository = productRepository;
+        this.productService = productService;
+    }
+
+    // Данный метод предназначен для отображении товаров без прохождения аутентификации и авторизации
+    @GetMapping("")
+    public String getAllProduct(Model model){
+        model.addAttribute("products", productService.getAllProduct());
+        return "product/product";
+    }
+
+    @GetMapping("/admin")
+    public String getAllProductAdm(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        String role = personDetails.getPerson().getRole();
+        if(role.equals("ROLE_USER")){
+            return "redirect:/index";
+        }
+        model.addAttribute("products", productService.getAllProductAdm());
+        return "product/product_adm";
+    }
+
+    @GetMapping("/index")
+    public String getAllProductUser(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        String role = personDetails.getPerson().getRole();
+        if(role.equals("ROLE_ADMIN")){
+            return "redirect:/admin";
+        }
+        model.addAttribute("products", productService.getAllProductUser());
+        return "product/product_user";
+    }
+
+    @GetMapping("/info/{id}")
+    public String infoProduct(@PathVariable("id") int id, Model model){
+        model.addAttribute("product", productService.getProductId(id));
+        return "product/infoProduct";
+    }
+
+    @PostMapping("/search")
+    public String productSearch(@RequestParam("search") String search, @RequestParam("ot") String ot, @RequestParam("do") String Do, @RequestParam(value = "price", required = false, defaultValue = "") String price, @RequestParam(value = "category", required = false, defaultValue = "") String category, Model model){
+        System.out.println(search);
+        System.out.println(ot);
+        System.out.println(Do);
+        System.out.println(price);
+        System.out.println(category);
+        // Если диапазон цен от и до не пустой
+        if(!ot.isEmpty() & !Do.isEmpty()) {
+            // Если сортировка по цене выбрана
+            if (!price.isEmpty()) {
+                // Если в качестве сортировки выбрана сортировкам по возрастанию
+                if (price.equals("sorted_by_ascending_price")) {
+                    // Если категория товара не пустая
+                    if (!category.isEmpty()) {
+                        // Если категория равная мебели
+                        if (category.equals("furniture")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
+                            // Если категория равная бытовой техники
+                        } else if (category.equals("clothes")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
+                            // Если категория равная одежде
+                        } else if (category.equals("pushkit")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
+                        } if (category.equals("drogue")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 4));
+                        }
+                        // Если категория не выбрана
+                    } else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+                    }
+                    // Если в качестве сортировки выбрана сортировкам по убыванию
+                } else if (price.equals("sorted_by_descending_price")) {
+                    // Если категория товара не пустая
+                    if (!category.isEmpty()) {
+                        // Если категория равная мебели
+                        if (category.equals("furniture")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
+                            // Если категория равная бытовой техники
+                        } else if (category.equals("clothes")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
+                            // Если категория равная одежде
+                        } else if (category.equals("pushkit")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
+                        } if (category.equals("drogue")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 4));
+                        }
+                        // Если категория не выбрана
+                    } else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+                    }
+                }
+            }
+            else {
+                model.addAttribute("search_product", productRepository.findByTitleAndPriceGreaterThanEqualAndPriceLessThan(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+            }
+        } else {
+            model.addAttribute("search_product",productRepository.findByTitleContainingIgnoreCase(search));
+        }
+        model.addAttribute("value_search", search);
+        model.addAttribute("value_price_ot", ot);
+        model.addAttribute("value_price_do", Do);
+        model.addAttribute("sorted_by_ascending_price",price);
+        model.addAttribute("sorted_by_descending_price",price);
+        model.addAttribute("furniture",category);
+        model.addAttribute("pushkit",category);
+        model.addAttribute("drogue",category);
+
+        model.addAttribute("products", productService.getAllProductAdm());
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        String role = personDetails.getPerson().getRole();
+        if(role.equals("ROLE_ADMIN")){
+            return "product/product_adm";
+        }else if (role.equals("ROLE_USER")) {
+            return "product/product_user";
+        }
+        return "product/product";
+    }
+
+    @PostMapping("/search_a")
+    public String productSearchA(@RequestParam("search_a") String search, @RequestParam("ot") String ot, @RequestParam("do") String Do, @RequestParam(value = "price", required = false, defaultValue = "") String price, @RequestParam(value = "category", required = false, defaultValue = "") String category, Model model){
+        System.out.println(search);
+        System.out.println(ot);
+        System.out.println(Do);
+        System.out.println(price);
+        System.out.println(category);
+        // Если диапазон цен от и до не пустой
+        if(!ot.isEmpty() & !Do.isEmpty()) {
+            // Если сортировка по цене выбрана
+            if (!price.isEmpty()) {
+                // Если в качестве сортировки выбрана сортировкам по возрастанию
+                if (price.equals("sorted_by_ascending_price")) {
+                    // Если категория товара не пустая
+                    if (!category.isEmpty()) {
+                        // Если категория равная мебели
+                        if (category.equals("furniture")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
+                            // Если категория равная бытовой техники
+                        } else if (category.equals("clothes")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
+                            // Если категория равная одежде
+                        } else if (category.equals("pushkit")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
+                        } else if (category.equals("drogue")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 4));
+                        }
+                        // Если категория не выбрана
+                    } else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPrice(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+                    }
+                    // Если в качестве сортировки выбрана сортировкам по убыванию
+                } else if (price.equals("sorted_by_descending_price")) {
+                    // Если категория товара не пустая
+                    if (!category.isEmpty()) {
+                        // Если категория равная мебели
+                        if (category.equals("furniture")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
+                            // Если категория равная бытовой техники
+                        } else if (category.equals("clothes")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
+                            // Если категория равная одежде
+                        } else if (category.equals("pushkit")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
+                        } else if (category.equals("drogue")) {
+                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 4));
+                        }
+                        // Если категория не выбрана
+                    } else {
+                        model.addAttribute("search_product", productRepository.findByTitleOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+                    }
+                }
+            }
+            else {
+                model.addAttribute("search_product", productRepository.findByTitleAndPriceGreaterThanEqualAndPriceLessThan(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+            }
+        } else {
+            model.addAttribute("search_product",productRepository.findByTitleContainingIgnoreCase(search));
+        }
+        model.addAttribute("value_search", search);
+        model.addAttribute("value_price_ot", ot);
+        model.addAttribute("value_price_do", Do);
+        model.addAttribute("sorted_by_ascending_price",price);
+        model.addAttribute("sorted_by_descending_price",price);
+        model.addAttribute("furniture",category);
+        model.addAttribute("appliances",category);
+        model.addAttribute("pushkit",category);
+        model.addAttribute("drogue",category);
+
+        model.addAttribute("products", productService.getAllProductAdm());
+        return "product/product";
+    }
+
+}
---
+++ shop/SpringSecurityApplication/src/main/java/com/example/springsecurityapplication/repositories/OrderRepository.java
@@ -0,0 +1,17 @@
+package com.example.springsecurityapplication.repositories;
+
+import com.example.springsecurityapplication.models.Order;
+import com.example.springsecurityapplication.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findByPerson(Person person);
+}
